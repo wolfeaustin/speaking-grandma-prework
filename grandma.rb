009a02340b688c159860b19095cf30bce74d7f4f
Austin Wolfe <wolfe_austin@yahoo.com>
2018-03-02 21:33:16 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,8 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
-  elsif phrase == ""
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    
 end 
 
 # Write a speak_to_grandma method.
