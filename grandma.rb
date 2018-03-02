f4227ca4d47fc2ed349ad4ff09fa967fb2bb8268
Austin Wolfe <wolfe_austin@yahoo.com>
2018-03-02 21:33:50 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,8 @@ def speak_to_grandma(phrase)
   elsif phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   else 
-    ""
+    "HUH?! SPEAK UP, SONNY!"
+  end 
 end 
 
 # Write a speak_to_grandma method.
